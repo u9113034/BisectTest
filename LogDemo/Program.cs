27bd3a44da89f7bd5bd67083b28571b523527f9b
Chou Kung Mao <u9113034@gmail.com>
2024-03-04 23:17:47 +0800

    using ccc

--- LogDemo/Program.cs
+++ LogDemo/Program.cs
@@ -13,6 +13,7 @@ namespace LogDemo
             Console.WriteLine("Hello world");
             Console.WriteLine("Hello world2");
             Console.WriteLine("Hello world3");
+            Console.WriteLine("Hello world4");
         }
     }
 }
