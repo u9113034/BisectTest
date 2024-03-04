79a14325e9bedad025b5aa1f45bc9b5a2a4b2ff6
Chou Kung Mao <u9113034@gmail.com>
2024-03-04 23:17:38 +0800

    using bbb

--- LogDemo/Program.cs
+++ LogDemo/Program.cs
@@ -10,7 +10,9 @@ namespace LogDemo
             var calculator = new Calculator();
             Console.WriteLine($"5+3 = {calculator.Add(5, 3)}");
             Console.WriteLine($"The square root of 5 = {calculator.SquareRoot(5.0)}");
-            Console.WriteLine('Hello world');
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world2");
+            Console.WriteLine("Hello world3");
         }
     }
 }
