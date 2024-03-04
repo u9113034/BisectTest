9decc29a8b7a80000b3be4a174ebed677afa4e58
Chou Kung Mao <u9113034@gmail.com>
2024-03-04 23:17:29 +0800

    using aaa

---
+++ LogDemo/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace UtilityKnife
+{
+	public class Calculator
+	{
+		public double Add(double x, double y)
+		{
+			return x + y;
+		}
+		public double Subtract(double x,double y)
+		{
+			return x - y;
+		}
+		public double Multiply(double x,double y)
+		{
+			return x * y;
+		}
+		public int Division(int x,int y)
+		{
+			return x / y;
+		}
+		public double Division(double x,double y)
+		{
+			return x / y;
+		}
+		public double SquareRoot(double x)
+		{
+			return Math.Sqrt(x);
+		}
+	}
+
+}
